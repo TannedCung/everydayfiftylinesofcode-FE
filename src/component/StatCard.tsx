8d7d9209eb501fe2ff06cf2ed28498659b2d1f75
TannedCung <tando1903@gmail.com>
2024-12-01 09:14:08 +0700

    Update commit Stats

--- src/component/StatCard.tsx
+++ src/component/StatCard.tsx
@@ -56,15 +56,15 @@ export default function StatCard({
   return (
     <Card variant="outlined" sx={{ height: '100%', flexGrow: 1 }}>
       <CardContent>
-        <Typography component="h2" variant="subtitle2" gutterBottom>
-          {title}
-        </Typography>
         <Stack
           direction="column"
           sx={{ justifyContent: 'space-between', flexGrow: '1', gap: 1 }}
         >
           <Stack sx={{ justifyContent: 'space-between' }}>
-            <Typography variant="h4" component="p">
+            <Typography variant="subtitle1" component="p">
+              {title} {/* This will display total commits or changes */}
+            </Typography>
+            <Typography variant="h6" component="p">
               {value} {/* This will display total commits or changes */}
             </Typography>
           </Stack>

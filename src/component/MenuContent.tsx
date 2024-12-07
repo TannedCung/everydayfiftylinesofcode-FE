e4912883f466a172e25a0df9237007f2ffe8b1a7
TannedCung <tando1903@gmail.com>
2024-12-07 09:27:39 +0700

    Mock tabs

--- src/component/MenuContent.tsx
+++ src/component/MenuContent.tsx
@@ -8,16 +8,14 @@ import Stack from '@mui/material/Stack';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import AnalyticsRoundedIcon from '@mui/icons-material/AnalyticsRounded';
 import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
-import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
 import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
 
 const mainListItems = [
   { text: 'Home', icon: <HomeRoundedIcon /> },
-  { text: 'Analytics', icon: <AnalyticsRoundedIcon /> },
-  { text: 'Clients', icon: <PeopleRoundedIcon /> },
-  { text: 'Tasks', icon: <AssignmentRoundedIcon /> },
+  { text: 'Challenges', icon: <AnalyticsRoundedIcon /> },
+  { text: 'Clubs', icon: <PeopleRoundedIcon /> },
 ];
 
 const secondaryListItems = [

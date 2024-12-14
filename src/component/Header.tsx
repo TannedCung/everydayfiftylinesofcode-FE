88c5423d03be7fabe41537ed50b0175e899d8612
TannedCung <tando1903@gmail.com>
2024-12-14 16:53:56 +0700

    Add: Padding to header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
         alignItems: { xs: 'flex-start', md: 'center' },
         justifyContent: 'space-between',
         maxWidth: { sm: '100%', md: '1700px' },
-        pt: 1.5,
+        padding: 1.5,
       }}
       spacing={2}
     >
